c1e6517899c344a87c3409706058665c66191514
Aman Singh Chauhan <120937044+amansingh1528@users.noreply.github.com>
2024-11-14 14:23:26 +0530

    Update resource_reccomendation_agent.py

--- agents/resource_reccomendation_agent.py
+++ agents/resource_reccomendation_agent.py
@@ -1,13 +1,3 @@
-
-use_cases = [
-    "Predictive Maintenance for AI-powered devices",
-    "Customer Service Automation with AI Chatbots",
-    "AI-based Fraud Detection in Financial Transactions",
-    "AI for Personalized Healthcare Recommendations",
-    "AI-driven Diagnostics and Imaging Solutions"
-]
-
-
 from agents.genai_usecase import generate_use_case, use_cases
 
 def recommend_resources(use_cases):
